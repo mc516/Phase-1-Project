18dcf80e22ea5ed4d99b3d90315dab913386afe1
Mickey <mickeyc11111@gmail.com>
2022-06-18 14:46:23 -0400

    findRandomActivity function for fetcg GET request for activity

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
+let randomBtn = document.querySelector('#random');
+randomBtn.addEventListener('click', findRandomActivity());
+    
+   
+
+
+
+
+
+
+
+
+function findRandomActivity() {
     fetch('http://www.boredapi.com/api/activity/')
     .then(res => res.json())
-    .then(data => console.log(data))
-})
+    .then(data => {
+        let randomAct = document.querySelector('.random-activity')
+        randomAct.innerHTML = `${data.activity}`
+    })
+}
