a74aaf4904a959395ecc4137b859130ed3becad2
Mickey <mickeyc11111@gmail.com>
2022-06-20 17:37:52 -0400

    event listener for Find an activity! button

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ document.addEventListener('DOMContentLoaded', () => {
     randomBtn.addEventListener('click', () => {
         findRandomActivity();
     });
+
+    let form = document.querySelector('form');
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        let filter = e.target.selector.value;
+        let filterActivity = filterActivity(filter);
+        let activity = document.querySelector('filtered-activity')
+        activity.innerHTML= `${activity.activity}`
+
+    })
+
+    let findActivityBtn = document.querySelector('.type-submit')
+    
 })
 
 
@@ -13,5 +26,17 @@ function findRandomActivity() {
     .then(data => {
         let randomAct = document.querySelector('.random-activity')
         randomAct.innerHTML = `${data.activity}`
+    })
+}
+
+function filterActivity(type) {
+    fetch('http://www.boredapi.com/api/activity/')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        if(data.type === type) {
+            console.log('yay')
+            return data.activity;
+        } else filterActivity(type)
     })
 }
