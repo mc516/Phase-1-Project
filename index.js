bbb2804e1d64082005d1dae0d3daa35f1596e36b
Mickey <mickeyc11111@gmail.com>
2022-06-20 20:06:43 -0400

    Somewhat working filterActivity function. Need to incorporate into DOM

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ document.addEventListener('DOMContentLoaded', () => {
     form.addEventListener('submit', e => {
         e.preventDefault();
         let filter = e.target.selector.value;
-        //let filtered = filterActivity(filter);
-        let activity = document.querySelector('filtered-activity')
+        let filtered = filterActivity(filter);
+        console.log(filter)
+        console.log(filtered)
+        //console.log(filtered.activity)
+        let activity = document.querySelector('.filtered-activity')
         activity.addEventListener('click', () => console.log('click'))
         //activity.innerHTML = filtered.activity
 
@@ -28,14 +31,18 @@ function findRandomActivity() {
 }
 
 function filterActivity(type) {
+    let result;
     fetch('http://www.boredapi.com/api/activity/')
     .then(res => res.json())
     .then(data => {
-        console.log(data)
-        if(data.type === type) {
-            return data.activity;
+        if(data.type == type) {
+            console.log(data)
+            result = data.activity;
         } else {
             filterActivity(type)
         }
+        console.log(result)
+        return result;
+        
     })
 }
