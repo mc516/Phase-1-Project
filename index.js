4a5ea15763da20b2c8616603971afd76bfe6dae7
Mickey <mickeyc11111@gmail.com>
2022-06-23 20:30:04 -0400

    removed clear list button and add to list from random activity

--- index.js
+++ index.js
@@ -22,12 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
         darkMode();
     })
 
-    let clearListBtn = document.querySelector('.clear-list')
-    //FOURTH EVENT LISTENER WITH ARR METHOD
-    clearListBtn.addEventListener('click', () => {
-       let list = document.querySelector('.list-activity')
-       list.innerHTML='';
-    })
+    
 
 })
 
@@ -38,14 +33,7 @@ function findRandomActivity() {
     .then(res => res.json())
     .then(data => {
         let randomAct = document.querySelector('.random-activity')
-        randomAct.innerHTML = `
-        ${data.activity}
-        <button class='add-to-list'>Add to List </button>
-        `
-        let addToListBtn = document.querySelector('.add-to-list')
-        addToListBtn.addEventListener('click', () => {
-            AddList(data);
-        })
+        randomAct.innerHTML = `${data.activity}`
     })
 }
 
@@ -71,21 +59,5 @@ function darkMode() {
     console.log(darkModeBtn)
 }
 
-function randomActList() {
-    fetch('http://www.boredapi.com/api/activity/')
-    .then(res => res.json())
-    .then(data => {
-        AddList(data)
-    })
-}
 
-function AddList(data) {
-   
-    arr.push(data.activity)
-    let li = document.createElement('ul')
-    li.innerHTML = `<h6>${data.activity}</h6>`
-    document.querySelector('.list-activity').appendChild(li)
 
-    console.log(arr)
-    
-}
